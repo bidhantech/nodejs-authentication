57fd19cde787bd46d2236717023e47e143761751
BIDHAN <53926488+bidhantech@users.noreply.github.com>
2021-10-03 13:12:19 +0530

    Project changed to node.js auth, will continue resume builder next time :disappointed_relieved:

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,15 +1,15 @@
 define({
-  "title": "Resume Builder API Docs",
+  "title": "Node.js Authentication Docs",
   "url": "http://localhost:3001/api",
-  "name": "resume-builder-api",
+  "name": "node.js-auth-api",
   "version": "0.0.1",
-  "description": "Resume Builder API",
+  "description": "Node.js Authentication API",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-07-25T07:35:55.669Z",
+    "time": "2021-10-03T07:40:03.230Z",
     "url": "https://apidocjs.com",
     "version": "0.28.1"
   }
